16f48cdd6638c8c36fcda405fd8672a58b1b7153
Sean Murphy <sean@iamseanmurphy.com>
2024-10-20 02:22:36 +0000

    4.1.5

---
+++ catch-evolution/inc/panel/js/customizer-custom-scripts.js
@@ -0,0 +1,21 @@
+/**
+ * Theme Customizer custom scripts
+ * Control of show/hide events on feature slider type selection
+ */
+
+//Change value of hidden field below custom checkboxes
+jQuery( document ).ready( function() {
+    jQuery( '.customize-control-catchevolution_custom_checkbox input[type="checkbox"]' ).on(
+        'change',
+        function() {
+        	checkbox_value = "0";
+
+            if ( jQuery( this ).is(":checked") ) {
+            	checkbox_value = "1";
+            }
+
+            jQuery( this ).parents( '.customize-control' ).find( 'input[type="hidden"]' ).val( checkbox_value ).trigger( 'change' );
+        }
+    );
+
+} ); // jQuery( document ).ready
